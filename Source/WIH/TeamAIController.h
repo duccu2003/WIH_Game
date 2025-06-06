2d6701c5a560b90bf5b5d58c192224c740f0efa1
Duc Nguyen <vippro11011@gmail.com>
2025-06-07 06:40:10 +0700

    update 250607

---
+++ Source/WIH/TeamAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TeamAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIH_API ATeamAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
